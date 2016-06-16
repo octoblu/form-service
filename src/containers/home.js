00b9ce5eb27645696f6de00816c6525ea5b8e91e
sqrt <sqrt@octoblu.com>
2016-06-15 17:09:44 -0700

    Store bearerToken and schemaUrl on willMount

--- src/containers/home.js
+++ src/containers/home.js
@@ -5,30 +5,36 @@ import {Spinner,ErrorState}  from 'zooid-ui'
 import Form  from '../components/Form'
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      error: null,
-      loading: true,
-      schema: null,
-    }
+  static initialState = {
+    error: null,
+    loading: null,
+    schema: null,
+    schemaUrl: null,
+    postUrl: null,
+    bearerToken: null,
   }
+  state = {...Home.initialState}
 
-  componentDidMount() {
+  componentDidMount = () => {
     this.fetchSchema()
   }
 
-  fetchSchema(){
+  componentWillMount = () => {
     const {schemaUrl, postUrl, bearerToken} = url.parse(location.href, true).query
+    this.setState({schemaUrl, postUrl, bearerToken})
+  }
+
+  fetchSchema = () => {
     this.setState({loading: true})
+
     superagent
-      .get(schemaUrl)
+      .get(this.state.schemaUrl)
       .set('Accept', 'application/json')
       .end((error, response) => {
         if (error) {
-          return this.setState({error, schema: null, loading: false})
+          return this.setState({...Home.initialState, error})
         }
-        this.setState({schema: JSON.parse(response.text), loading: false, postUrl: postUrl})
+        this.setState({schema: JSON.parse(response.text), loading: false})
       })
   }
 
@@ -41,13 +47,12 @@ export default class Home extends React.Component {
       .send(model)
       .end((error, response) => {
         if (error) return this.setState({error, schema: null, loading: false})
-    
-        if (!response.headers.location) return this.setState({error, loading: false})
+
         window.location = response.headers.location
       })
   }
 
-  render() {
+  render = () => {
     const {schema, postUrl, loading, error} = this.state
     if (loading) return <Spinner />
     if (!schema || !postUrl) return <ErrorState description="Sorry! Looks like we couldn't find your schema or postUrl!" />
